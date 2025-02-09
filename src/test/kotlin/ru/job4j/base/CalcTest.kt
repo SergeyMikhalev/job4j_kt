c4134bed78a9467ac1598d1f4ab18cf6bf0dfeec
Сергей <tolltroll@yandex.ru>
2025-02-09 13:00:46 +0300

    branch - 189221 - Операторы условий if

--- src/test/kotlin/ru/job4j/base/CalcTest.kt
+++ src/test/kotlin/ru/job4j/base/CalcTest.kt
@@ -25,4 +25,30 @@ class CalcTest {
     fun divTest() {
         assertThat(divide(9,3)).isEqualTo(3)
     }
+
+    @Test
+    fun max2AndFirstGreater() {
+        assertThat(max(66,11)).isEqualTo(66)
+    }
+
+    @Test
+    fun max2AndSecondGreater() {
+        assertThat(max(9,14)).isEqualTo(14)
+    }
+
+    @Test
+    fun max3AndFirstGreater() {
+        assertThat(max(7,1,4)).isEqualTo(7)
+    }
+
+    @Test
+    fun max3AndSecondGreater() {
+        assertThat(max(66,111,0)).isEqualTo(111)
+    }
+
+    @Test
+    fun max3AndThirdGreater() {
+        assertThat(max(-33,5,10)).isEqualTo(10)
+    }
+
 }

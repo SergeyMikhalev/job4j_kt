9836906b12f97da00cafd5dd785dd172896681af
Сергей <tolltroll@yandex.ru>
2025-02-16 19:15:07 +0300

    branch - 505332 add detekt and fix some code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
 }
 
 group = "ru.job4j"
@@ -13,6 +14,21 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.1")
     testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+detekt {
+    toolVersion = "1.23.6" // Версия Detekt
+    config = files("$projectDir/config/detekt/detekt.yml")
+    buildUponDefaultConfig = true // Настройки будут расширять дефолтный конфиг Detekt
+    allRules = false // Использовать только правила, указанные в конфиге
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt> {
+    reports {
+        html.required.set(true) // Отчет в формате HTML
+        xml.required.set(true)  // Отчет в формате XML
+        txt.required.set(false) // Текстовый отчет
+    }
 }
 
 tasks.test {
--- src/main/kotlin/ru/job4j/base/Calc.kt
+++ src/main/kotlin/ru/job4j/base/Calc.kt
@@ -23,24 +23,42 @@ fun max(first:Int, second: Int, third: Int): Int
 = max(max(first, second), third)
 
 
+private const val FIVE = 5
+
+private const val FOUR = 4
+
+private const val SEVEN = 7
+
+private const val THREE = 3
+
+private const val EIGHT = 8
+
+private const val SIX = 6
+
+private const val TWO = 2
+
+private const val NINE = 9
+
+private const val ELEVEN = 11
+
 fun main() {
 
-    val plus = add(5, 4)
+    val plus = add(FIVE, FOUR)
     println("5 + 4 = $plus")
 
-    val minus = subtract(7, 3)
+    val minus = subtract(SEVEN, THREE)
     println("7 - 3 = $minus")
 
-    val mul = multiply(7, 8)
+    val mul = multiply(SEVEN, EIGHT)
     println("7 * 8 = $mul")
 
-    val div = divide(6, 2)
+    val div = divide(SIX, TWO)
     println("6 / 2 = $div")
 
-    val maximum = max(7, 4)
+    val maximum = max(SEVEN, FOUR)
     println("max from 7 and 4 is $maximum")
 
-    val max3 = max(9, 11,2)
+    val max3 = max(NINE, ELEVEN, TWO)
     println("max from 9, 11, 2 is $max3")
 
 }

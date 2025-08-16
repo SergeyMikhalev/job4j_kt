22e3d03011da78cfbbb2ae9d5c19503dc846c666
Сергей <tolltroll@yandex.ru>
2025-08-16 18:34:39 +0300

    branch - 191436 Переопределение

--- src/main/kotlin/ru/job4j/oop/Professions.kt
+++ src/main/kotlin/ru/job4j/oop/Professions.kt
@@ -1,11 +1,33 @@
 package ru.job4j.oop
 
-open class Profession(val salary: Int = 0)
+open class Profession(val salary: Int = 0) {
+    open fun getName():String = "Profession"
 
-class Doctor(salary: Int, val speciality: String = "Therapist"): Profession(salary)
+    open fun action() {
+        println("action from " + getName())
+    }
+}
+
+class Doctor(salary: Int, val speciality: String = "Therapist"): Profession(salary) {
+    override fun getName(): String = "Doctor"
+
+    override fun action() {
+        super.action()
+        println("Healing injures")
+        println()
+    }
+}
 
 @Suppress("detekt:UnusedPrivateProperty")
-class Policeman(salary: Int, shoeSize:Int = 42): Profession(salary)
+class Policeman(salary: Int, shoeSize:Int = 42): Profession(salary) {
+    override fun getName(): String = "Policeman"
+
+    override fun action() {
+        super.action()
+        println("Capturing bad boys")
+        println()
+    }
+}
 
 private const val ONE_THOUSAND = 1000
 
@@ -14,6 +36,6 @@ private const val TWO_THOUSAND = 2000
 private const val FORTY_FOUR = 44
 
 fun main(args: Array<String>) {
-    println(Doctor(ONE_THOUSAND))
-    println(Policeman(TWO_THOUSAND, FORTY_FOUR))
+    Doctor(ONE_THOUSAND).action()
+    Policeman(TWO_THOUSAND, FORTY_FOUR).action()
 }

346050790f5d0807577c61cdbcf80509b1f5deab
SergeyMikhalev <33098478+SergeyMikhalev@users.noreply.github.com>
2025-08-18 21:43:40 +0300

    branch - 338438 Лямбды №1 (#15)
    * branch - 338438 Лямбды №1
    
    * branch - 338438 Лямбды №1
    
    * branch - 338438 Лямбды №1

---
+++ src/main/kotlin/ru/job4j/lambda/LambdaFunctions.kt
@@ -0,0 +1,16 @@
+package ru.job4j.lambda
+
+@Suppress("detekt:MagicNumber")
+fun main() {
+    val max = { x: Int, y: Int -> if (x > y) x else y }
+    val decrement = { x: Int -> x - 1 }
+    val square = { x: Int -> x * x }
+
+    val resultMax = max(1, 2)
+    val resultDecrement = decrement(11)
+    val resultSquare = square(5)
+
+    println(resultMax)
+    println(resultDecrement)
+    println(resultSquare)
+}

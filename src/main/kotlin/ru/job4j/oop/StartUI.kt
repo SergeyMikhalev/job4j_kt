0e6ef3da3b765060d0a043851dc30d5c08559e4f
SergeyMikhalev <33098478+SergeyMikhalev@users.noreply.github.com>
2025-08-18 21:42:52 +0300

    branch - 191438 Интерфейсы (#13)

--- src/main/kotlin/ru/job4j/oop/StartUI.kt
+++ src/main/kotlin/ru/job4j/oop/StartUI.kt
@@ -1,45 +1,41 @@
 package ru.job4j.oop
 
+import ru.job4j.oop.action.AddItemAction
+import ru.job4j.oop.action.ExitApplicationAction
+import ru.job4j.oop.action.PrintAllItemsAction
+import ru.job4j.oop.input.ConsoleInput
+
+private const val OPERATIONS = """
+                        ----------------------------------
+                        Доступные операции:
+                        1 - Добавить заявку
+                        2 - Вывести список заявок 
+                        3 - Выход
+                        Пожалуйста, введите номер операции:
+                    """
+
 class StartUI {
 
-    companion object {
-        
-        private const val OPERATIONS_DESCRIPTION = """
-                ----------------------------------
-                Доступные операции:
-                1 - Добавить заявку
-                2 - Вывести список заявок 
-                3 - Выход
-                Пожалуйста, введите номер операции:
-            """
-
-        fun printAllItems(tracker: Tracker) {
-            println("Список сохраненных заявок:")
-            tracker.findAll().forEach { println("" + it.id + " - " + it.name) }
-        }
-
-        fun addItem(tracker: Tracker) {
-            println("Введите пожалуйста имя заявки:")
-            tracker.add(Item(0, readln()))
-        }
-    }
-
     private val tracker = Tracker()
+    private val input = ConsoleInput()
+
+    private val addItemAction = AddItemAction()
+    private val printAllItemsAction = PrintAllItemsAction()
+    private val exitApplicationAction = ExitApplicationAction()
 
     fun init() {
         var run = true
         var operation =""
         while (run) {
-            println(OPERATIONS_DESCRIPTION.trimIndent())
+            println(
+                OPERATIONS.trimIndent()
+            )
             operation = readln()
 
             when (operation) {
-                "1" -> addItem(tracker)
-                "2" -> printAllItems(tracker)
-                "3" -> {
-                    println("Завершение работы!")
-                    run = false
-                }
+                "1" -> run = addItemAction.execute(tracker, input)
+                "2" -> run = printAllItemsAction.execute(tracker, input)
+                "3" -> run = exitApplicationAction.execute(tracker, input)
                 else -> println("Неверный ввод. Повсторите попытку.")
             }
         }

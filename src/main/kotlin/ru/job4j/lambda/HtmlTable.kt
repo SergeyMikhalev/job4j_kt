f625ccf12bc2d4352d9ec9ff9bed07dc5407e7b3
SergeyMikhalev <33098478+SergeyMikhalev@users.noreply.github.com>
2025-08-27 16:49:55 +0300

    Branch 338631 (#19)
    * branch - 338631 Лямбды Apply
    
    * branch - 338631 Лямбды Apply
    
    * branch - 338631 Лямбды Apply
    
    * branch - 338631 Лямбды Apply
    
    * branch - 338631 Лямбды Apply

---
+++ src/main/kotlin/ru/job4j/lambda/HtmlTable.kt
@@ -0,0 +1,25 @@
+package ru.job4j.lambda
+
+@SuppressWarnings("detekt:UnusedPrivateProperty")
+class HtmlTable {
+    fun table(row: Int, cell: Int): String {
+        val table = StringBuilder()
+        table.apply {
+            append("<table>\n")
+            for (rows in 0 .. row) {
+                append("<tr> ")
+                for (cells in 0 .. cell) {
+                    append("<td></td> ")
+                }
+                append("</tr>\n")
+            }
+            append("</table>\n")
+        }
+        return table.toString()
+    }
+}
+
+@SuppressWarnings("detekt:MagicNumber")
+fun main() {
+    println(HtmlTable().table(3,3))
+}

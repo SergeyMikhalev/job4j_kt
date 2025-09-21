f915e2576db7532ee3682bf337f5a3a2d6ab4864
Сергей <tolltroll@yandex.ru>
2025-09-21 20:59:19 +0300

    branch - 505325  Отмена корутин

--- src/main/kotlin/ru/job4j/coroutines/DataService3.kt
+++ src/main/kotlin/ru/job4j/coroutines/DataService3.kt
@@ -1,33 +1,49 @@
 package ru.job4j.coroutines
 
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.launch
+import kotlin.coroutines.cancellation.CancellationException
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+
 
 class DataService3 {
 
-    suspend fun performOperation(timeout: Long) {
+    @SuppressWarnings("detekt:MagicNumber")
+    suspend fun performOperation(timeout: Long) = coroutineScope {
 
-        val job = la
+        val usefulJob = launch {
+            try {
+                for (i in 1..3) {
+                    ensureActive()
+                    val duration = 1000 * (1..3).random().toLong()
+                    println("Корутина 1: Делаю полезную работу. Часть $i. Время выполнения: $duration мс.")
+                    delay(duration)
+                }
+                println("Корутина 1: Всё сделал")
+            } catch (e :CancellationException) {
+                println("Корутина 1: Корутина прервана извне")
+                throw e
+            }
 
-        // Создайте первую корутину для выполнения полезной функции (job)
-        // В этой корутине используйте цикл и delay для имитации работы
-        // Добавьте логику для обработки отмены через try-catch или isActive
+        }
 
-        // Создайте вторую корутину для отслеживания времени и отмены первой корутины
-        // Используйте delay для имитации тайм-аута
-        // Если тайм-аут достигнут, отмените первую корутину
+        launch {
+            delay(timeout)
+            if (usefulJob.isActive) {
+                println("Корутина 2: Превышен лимит времени выполнения полезной работы. Отмена корутины.")
+                usefulJob.cancel()
+            }
+        }
 
-        // Ожидайте завершения первой корутины с помощью job.join()
+        usefulJob.join()
     }
 
 }
 
+@SuppressWarnings("detekt:MagicNumber")
 fun main() = runBlocking {
     val dataService = DataService3()
-    dataService.performOperation(1000)
-
-    val some = launch {
-        println()
-    }
+    dataService.performOperation(5000)
 }
